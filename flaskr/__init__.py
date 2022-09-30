83cbbea320bfff3bee8d1706cddceea2465e770d
Arnav Rustagi <arnrustagi@gmail.com>
2022-10-01 03:46:43 +0530

    api saving the tts file to ./static/tts/.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,7 @@
 import os
 from time import time_ns
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 # text to speech module
 from gtts import gTTS
@@ -9,13 +9,28 @@ from gtts import gTTS
 app = Flask(__name__)
 
 # this is the api endpoint for the tts function
-@app.route('/api/text-to-speech',methods=['POST','GET'])
+@app.route('/api/text-to-speech',methods=['POST'])
 def tts_api_endpoint ():
-#    data = request.get_json()
-#    print(data)
-    return '<h1>Hello World</h1>'
+    data = request.get_json()
 
-def tts (text, lang='en', slow='False'):
+    DATA = tts(
+            text=data.pop("src",None),
+            lang=data.pop("lang",None),
+            slow=data.pop("slow",None)
+        )
+
+    return jsonify(DATA)
+
+def tts (text, lang='en', slow=False):
+    lang = lang  if lang  else 'en'
+    slow = False if slow else True
+
     uid = time_ns()
     gtts_object = gTTS(text=text,lang=lang,slow=slow)
-    gtts.save(f'./static/tts/{uid}.mp3')
+
+    print(os.getcwd())
+    gtts_object.save(f'./flaskr/static/tts/{uid}.mp3')
+
+    DATA = {'filename' : uid}
+
+    return DATA
--- temp.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-import os
-
-txt = 'Hey whats up riya'
-lang = 'en'
-
-tts = gTTS(text=txt,lang=lang,slow=True)
-tts.save('./temp.mp3')
